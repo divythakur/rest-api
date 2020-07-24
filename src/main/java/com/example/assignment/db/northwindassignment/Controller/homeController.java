c2533340b39b9ca8f2f9d3430708463a161323ec
divythakur <divyanshuthakur75@gmail.com>
2020-07-24 08:39:48 +0530

    homepage added

---
+++ src/main/java/com/example/assignment/db/northwindassignment/Controller/homeController.java
@@ -0,0 +1,15 @@
+package com.example.assignment.db.northwindassignment.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+@RequestMapping("/")
+    public String homepage()
+  {
+      return "Welcome  to   the   page   :-   As per  cloud   Condition   . Due to   Size  limitation  Some of the Data can be missed   so   plz ignore if there is mismatch with actual dataset  ..   THANK YOU"
+
+   }
+
+}
