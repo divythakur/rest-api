3b91417b1e592f4a877ec7bfe895ba963c839fcd
divythakur <divyanshuthakur75@gmail.com>
2020-07-25 20:06:14 +0530

    done

--- src/main/java/com/example/assignment/db/northwindassignment/Controller/HomeController.java
+++ src/main/java/com/example/assignment/db/northwindassignment/Controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     @RequestMapping("/")
     public String home()
     {
-        return "WELCOME . Note:-Use POST method and  DELETE method in postman instead of browser";
+        return "WELCOME . Note:-Use POST method and  DELETE method in postman instead of browser   CHECK the API Documentation to  use the API's";
     }
 }
